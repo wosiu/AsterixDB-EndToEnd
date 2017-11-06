7652e987ff6eb02e17da5632b7ee6a6d5010f0b6
Michal Wos <mw792829570@gmail.com>
2017-11-06 19:06:07 +0100

    query in while loop

--- query.sh
+++ query.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+	(>&2 echo "STOPPING..")
+	$PROJECT_HOME/scripts/stop_asterix.sh 1>/dev/null 2>/dev/null || { echo "Error while stopping asterix"; exit 1; }
+	exit 0
+}
+
 set -e
 
 if [ "$PROJECT_HOME" == "" ]
@@ -10,14 +19,18 @@ then
 fi
 
 INSTALL_ENV="$PROJECT_HOME/env"
+INTERVAL=5s
 
 source $INSTALL_ENV/bin/activate
 
 pushd "$PROJECT_HOME/scripts" > /dev/null
-	./start_asterix.sh || { echo "Error while starting asterix"; exit 1; }
-	# todo while true, sleep	
-	./single_query.sh $1 || { echo "Error while quering"; exit 1; }
-	./stop_asterix.sh || { echo "Error while stopping asterix"; exit 1; }
-popd > /dev/null
+	./start_asterix.sh 1>/dev/null 2>/dev/null || { echo "Error while starting asterix"; exit 1; }
+	# todo while true, sleep
+	while true
+	do	
+		(>&2 date +"%F %T")
+		./single_query.sh $1 || { echo "Error while quering"; exit 1; }
+		sleep $INTERVAL	
+	done
 
-deactivate
+
