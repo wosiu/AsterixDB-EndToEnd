5b5d01efcb4306f416e70d75a4ee7ba45d317877
Michal Wos <mw792829570@gmail.com>
2017-11-06 14:26:05 +0100

    stop query script if error

--- query.sh
+++ query.sh
@@ -14,10 +14,10 @@ INSTALL_ENV="$PROJECT_HOME/env"
 source $INSTALL_ENV/bin/activate
 
 pushd "$PROJECT_HOME/scripts" > /dev/null
-	./start_asterix.sh
+	./start_asterix.sh || { echo "Error while starting asterix"; exit 1; }
 	# todo while true, sleep	
-	./single_query.sh $1
-	./stop_asterix.sh
+	./single_query.sh $1 || { echo "Error while quering"; exit 1; }
+	./stop_asterix.sh || { echo "Error while stopping asterix"; exit 1; }
 popd > /dev/null
 
 deactivate
